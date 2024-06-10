056cc89c48c1650e562ad1dfc59d9ab850326e1e
ssetsofia <setsofia@washnote.com>
2024-06-10 20:38:07 +0000

    models created

--- hicoApp/models.py
+++ hicoApp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=2000)
+    pub_date = models.DateTimeField("date published")
+
+
+class Mission(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=2000)
+    soulswon = models.IntegerField
+    pub_date = models.DateTimeField("date published")
+
+
+class Widows(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=2000)
+    widowsfed = models.IntegerField
+    pub_date = models.DateTimeField("date published")
+
+
